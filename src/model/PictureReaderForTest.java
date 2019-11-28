4326a90b80c03d332b3e9a43b752730baecf360b
Ditte Kristina Bang <ditb@itu.dk>
2019-11-28 22:22:24 +0100

    PictureReader for testing purposes

--- src/model/PictureReaderForTest.java
+++ src/model/PictureReaderForTest.java
@@ -1,10 +1,12 @@
 package model;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class PictureReaderForTest {
     public static void main (String avg[]) {
@@ -14,15 +16,25 @@ public class PictureReaderForTest {
     try {
         File folder = new File("resources/pics/Film");
         File[] pics = folder.listFiles();
+        String title;
         for (File f : pics) {
             if (f.isFile()) {
                 photo = ImageIO.read(f);
                 picsHere.add(photo);
+                System.out.println("Cover for: " + f.getName());
             }
         }
     }
     catch(IOException e) {
     }
+    int i = 0;
+    System.out.println("TEST 1: ");
+
+            /*int i = 0;
+            ImageIcon image = new ImageIcon(picsHere.get(i));
+            JLabel label = new JLabel("Picture 0", image, JLabel.CENTER);
+            JPanel panel = new JPanel(new BorderLayout());
+            panel.add( label, BorderLayout.CENTER );*/
     }
 }
 
