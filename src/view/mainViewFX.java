19edd241d4a222cc1760e1e66ff00541facbcc11
esma <esma@itu.dk>
2019-11-28 17:17:05 +0100

    separeret dem i model og view og laver javaFX og Swing eksempler

---
+++ src/view/mainViewFX.java
@@ -0,0 +1,20 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class mainViewFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    }
+}
---
+++ src/view/mainViewSwing.java
@@ -0,0 +1,29 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class mainViewSwing extends JFrame {
+
+    public mainViewSwing(){
+        super("mainView");
+        makeFrame();
+    }
+
+    private void makeFrame(){
+        Container pane = getContentPane();
+        JLabel label = new JLabel("hejsa");
+        pane.add(label);
+
+        JButton button = new JButton("en knap");
+        JButton button2 = new JButton("en anden knap");
+        button.addActionListener(e -> System.out.println("KLIK"));
+        button2.addActionListener(e -> System.out.println("Klik2"));
+        pane.add(button);
+
+        pack();
+        setVisible(true);
+    }
+
+}
