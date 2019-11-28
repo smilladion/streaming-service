3506126796144aab83cba1b00f7d40319af0f133
Ditte Kristina Bang <ditb@itu.dk>
2019-11-28 19:15:04 +0100

    PictureReader for testing purposes

---
+++ src/PictureReaderForTest.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+
+public class PictureReaderForTest {
+    public static void main (String avg[]) throws IOException {
+    }
+
+    public PictureReaderForTest() throws IOException {
+        ArrayList<Image> pictures = new ArrayList<>();  //Opretter array
+        BufferedImage img = ImageIO.read(new File("resources/pics/Film/12 Angry Men")); //Finder specifik fil
+        pictures.add(img); //Tilføjer specificerede fil
+    }
+}
