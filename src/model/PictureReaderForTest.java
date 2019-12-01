13060fbedc56a355661a4f0a06c47d9421cb746c
edbma <esma@itu.dk>
2019-12-01 15:09:03 +0100

    rettet i koden, så vi har showHash(), loadHash() og main metoden som kalder begge metoder. har også lavet konstruktør for klassen

--- src/model/PictureReaderForTest.java
+++ src/model/PictureReaderForTest.java
@@ -12,33 +12,69 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class PictureReaderForTest {
-    public static void main (String avg[]) {
-    BufferedImage photo = null;
-    Map<String, BufferedImage> picFolder = new HashMap<>(1000);
+    public static BufferedImage photo = null;
+    public static Map<String, BufferedImage> picFolder = new HashMap<>(1000);
 
+    public PictureReaderForTest(){
+        photo = null;
+        picFolder = new HashMap<>(1000);
+    }
 
-    try {
-        File folder = new File("resources/pics/Film");
-        File[] pics = folder.listFiles();
-        String picTitle;
-        for (File f : pics) {
-            if (f.isFile()) {
-                photo = ImageIO.read(f);
-                picTitle = f.getName();
-                picFolder.put(picTitle,photo);
-                System.out.println("Cover for: " + f.getName()); //Tester bare et udprint af listen, skal self ikke med i den endelige kode
+    public static void loadHash() throws IOException {
+            File folder = new File("resources/pics/Film");
+            File[] pics = folder.listFiles();
+            String picTitle;
+            for (File f : pics) {
+                if (f.isFile()) {
+                    photo = ImageIO.read(f);
+                    picTitle = f.getName();
+                    picFolder.put(picTitle,photo);
+                    System.out.println("Cover for: " + f.getName()); //Tester bare et udprint af listen, skal self ikke med i den endelige kode
+                }
             }
         }
+
+    public static void showHash(){
+        JFrame frame = new JFrame();
+        frame.getContentPane().setLayout(new FlowLayout()); //kreerer flowlayout jframe
+
+        for(Map.Entry<String, BufferedImage> e : picFolder.entrySet()){ //tilføjer hvert billede til JFrame
+            String key = e.getKey();
+            BufferedImage value = e.getValue();
+            JLabel image = new JLabel(new ImageIcon(value));
+            image.setText(key);
+            image.setHorizontalTextPosition(SwingConstants.CENTER);
+            image.setVerticalTextPosition(SwingConstants.BOTTOM);
+            frame.getContentPane().add(image);
+
+            //String one = "12 Angry Men.jpg"; //en string
+            //picFolder.get(one);//giver det ene billede tilhørende den String
+            //JLabel image = new JLabel(key,new ImageIcon(value),SwingConstants.CENTER);
+            //ovenstående opretter JLabel med tekst,image,tekstposition
+        }
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack(); //viser det.
+        frame.setVisible(true);
+    }
+
+
+    public static void main (String avg[]) throws IOException {
+    try {
+        loadHash();
+        showHash();
     }
     catch(IOException e) {
     }
+    }
+}
+
     //Tester for at bekræfte om en titel er blevet lagret i hashmappet - den returner true, så det er den :)
-    System.out.println("TEST 1: "+picFolder.containsKey("12 Angry Men.jpg"));
+    //System.out.println("TEST 1: "+picFolder.containsKey("12 Angry Men.jpg"));
 
     //TEST FOR AT FÅ VIST BILLED FRA HASHMAP HERFRA:
         //TODO ryk det her over i view på et senere tidspunkt, så det køres defra.
         //public void forEachHash() //obs viser 100 billeder. USE WITH CAUTION!!!!!!
-        JFrame frame = new JFrame();
+        /*JFrame frame = new JFrame();
         frame.getContentPane().setLayout(new FlowLayout()); //kreerer flowlayout jframe
 
             for(Map.Entry<String, BufferedImage> e : picFolder.entrySet()){ //tilføjer hvert billede til JFrame
