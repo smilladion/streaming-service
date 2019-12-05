acf55f52eb66c23622157cd78396723eb2da27a6
Smilla <smdi@itu.dk>
2019-12-05 17:11:10 +0100

    Remove duplicate buttonpanel

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Display display = new Display();
         display.showButtons();
-        display.showFavourites();
         display.showMedia();
-        display.showButtons();
-
     }
 }
