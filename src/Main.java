3ea08de5b01b66834c54f2ac41b337d11ba3a96f
edbma <esma@itu.dk>
2019-12-05 15:26:08 +0100

    Merge branch 'master' of C:\Users\edbma\IdeaProjects\streaming-service with conflicts.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Display display = new Display();
+        display.showButtons();
         display.showFavourites();
         display.showMedia();
-        display.showButtons();
 
     }
 }
