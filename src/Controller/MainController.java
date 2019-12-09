0b7b441bffdb2c492a081bf5c6922e7677ee51a5
edbma <esma@itu.dk>
2019-12-09 17:50:32 +0100

    lavet en hjemmelavet unchecked expcetion som kastes hvis man prøver at tilføje en film flere gange til favoritter

---
+++ src/Controller/MainController.java
@@ -0,0 +1,9 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import view.Display;
+
+public class MainController {
+}
---
+++ src/model/MediaAlreadyAFavouriteException.java
@@ -0,0 +1,7 @@
+package model;
+
+public class MediaAlreadyAFavouriteException extends RuntimeException {
+    public MediaAlreadyAFavouriteException(){
+        super("This media has already been added to your favourites list");
+    }
+}
